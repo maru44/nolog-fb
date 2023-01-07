1dcba75493ec0ac74f2f19945bd24fa637d8d514
maru44 <takumaru22244@gmail.com>
2023-01-07 17:29:46 +0900

    public env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const blogDatabaseId = process.env.BLOG_DATABASE_ID ?? ''
 export const indieDatabaseId = process.env.INDIE_DATABASE_ID ?? ''
-export const publicStorage = process.env.PUBLIC_STORAGE
+export const publicStorage = process.env.NEXT_PUBLIC_PUBLIC_STORAGE
 
 export const getStorageURL = (name: string) => {
   if (name.startsWith('/')) return `${publicStorage}${name}`
