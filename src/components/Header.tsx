3393b9189c7a301949fcb9b14acf2ae6b54cc0a3
maru44 <takumaru22244@gmail.com>
2023-01-07 19:40:19 +0900

    fix ga

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,10 +18,6 @@ export const Header = ({ current = 'home' }: HeaderProps) => {
       <p>
         {current === 'home' && 'A libertarian. An web engineer.'}
         {current === 'indie' && 'Indie Works'}
-        {/* This is an example of a Next.js blog with data fetched with Notions API. The data comes from{' '}
-      <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get the source code on{' '}
-      <a href="https://github.com/samuelkraft/notion-blog-nextjs">Github</a> or read{' '}
-      <a href="https://samuelkraft.com/blog/building-a-notion-blog-with-public-api">my blogpost</a> on building your own. */}
       </p>
       <div className={styles.accounts}>
         <div>
