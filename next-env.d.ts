5949691e63489ccbb563e4b75beb117eebfdde90
maru44 <takumaru22244@gmail.com>
2023-01-09 12:15:31 +0900

    nextenv.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+interface Window {
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string
+      event_category: string
+      value?: number
+    }
+  )
+}
