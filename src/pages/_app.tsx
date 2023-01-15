1d1477933a5644bc7f2747f97467979e1e2d689c
maru44 <takumaru22244@gmail.com>
2023-01-15 17:47:04 +0900

    favicn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import { usePageView } from 'src/src/hooks/usePageView'
@@ -8,6 +9,10 @@ export default function App({ Component, pageProps }: AppProps) {
   usePageView()
   return (
     <>
+      <Head>
+        <link rel="canonical" href="https://maru44.web.app" />
+        <link rel="shortcut icon" type="image/jpg" href="/kilroy_sq.jpg" />
+      </Head>
       <div className="container">
         {pageProps.page && <Header page={pageProps.page} />}
         <Component {...pageProps} />
